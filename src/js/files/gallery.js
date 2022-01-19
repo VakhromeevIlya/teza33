aac37893c1330c10a8b5d1e22a63ac3f552adbe7
ilyaVahromeev <ilya.vahromeev@widestudio.ru>
2022-01-19 17:40:41 +0300

    make a few pages

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -10,20 +10,23 @@
 import lightGallery from 'lightgallery';
 // Плагины
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
-//import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.es5.js'
+import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.es5.js';
+import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.umd.js';
+import lgFullscreen from 'lightgallery/plugins/fullscreen/lg-fullscreen.es5.js';
+import lgRotate from 'lightgallery/plugins/rotate/lg-rotate.es5.js';
 
 // Базовые стили
 import '@scss/libs/gallery/lightgallery.scss';
 // Стили дополнений
-// import '@scss/libs/gallery/lg-thumbnail.scss';
+import '@scss/libs/gallery/lg-thumbnail.scss';
 // import '@scss/libs/gallery/lg-video.scss';
 // import '@scss/libs/gallery/lg-autoplay.scss';
-//import '@scss/libs/gallery/lg-zoom.scss';
+import '@scss/libs/gallery/lg-zoom.scss';
 // import '@scss/libs/gallery/lg-pager.scss';
-// import '@scss/libs/gallery/lg-fullscreen.scss';
+import '@scss/libs/gallery/lg-fullscreen.scss';
 // import '@scss/libs/gallery/lg-share.scss';
 // import '@scss/libs/gallery/lg-comments.scss';s
-// import '@scss/libs/gallery/lg-rotate.scss';
+import '@scss/libs/gallery/lg-rotate.scss';
 // import '@scss/libs/gallery/lg-medium-zoom.scss';
 // import '@scss/libs/gallery/lg-relative-caption.scss';
 
@@ -35,7 +38,12 @@ const galleries = document.querySelectorAll('[data-gallery]');
 if (galleries.length) {
 	galleries.forEach(gallery => {
 		lightGallery(gallery, {
-			//plugins: [lgZoom],
+			plugins: [lgZoom, lgFullscreen, lgThumbnail, lgRotate],
+			thumbnail: true,
+			fullScreen: true,
+			selector: "[data-gallery-item]",
+			zoom: true,
+			rotate: true,
 			licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 			speed: 500,
 		});
